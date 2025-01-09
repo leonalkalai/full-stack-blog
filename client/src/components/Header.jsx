c57942426efeb6f721344a4e5a54758bbedf72d3
leonalkalai <koundouras@gmail.com>
2025-01-09 09:30:32 +0200

    added useLocation hook for menu link paths and changed parent links to divs to avoid errors

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navbar, TextInput, Button, Flowbite } from "flowbite-react";
 import { AiOutlineSearch } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { IconContext } from "react-icons";
 
@@ -15,6 +15,7 @@ const customTheme = {
 };
 
 export default function Header() {
+  const path = useLocation().pathname;
   return (
     <Navbar>
       <Link
@@ -62,13 +63,13 @@ export default function Header() {
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link>
+        <Navbar.Link active={path === "/"} as={"div"}>
           <Link to="/">Home</Link>
         </Navbar.Link>
-        <Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
           <Link to="/about">About</Link>
         </Navbar.Link>
-        <Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
           <Link to="/projects">Projects</Link>
         </Navbar.Link>
       </Navbar.Collapse>
