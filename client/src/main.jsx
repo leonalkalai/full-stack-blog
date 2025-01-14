394748b05d0d5f2cb85a2bf6bd579237b3baf86c
leonalkalai <koundouras@gmail.com>
2025-01-14 10:51:00 +0200

    added redux persist to keep the data on refresh page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { store } from "./redux/store";
+import { store, persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
+/*
+ use persisted store from redux toolkit
+  https://www.dhiwise.com/post/how-to-use-redux-persist-in-react-applications
+*/
 createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,40 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit"; // import reducers combiner
 import userReducer from "./user/userSlice";
+import { persistReducer, persistStore } from "redux-persist"; // import persisted reducer and persisted store
+import storage from "redux-persist/lib/storage"; // import storage for localstorage
+
+/* 
+  Save the state in persistent storage so that even after a refresh, the data will remain intact.
+  https://blog.logrocket.com/persist-state-redux-persist-redux-toolkit-react/
+
+  Combine reducers
+  https://redux.js.org/api/combinereducers
+
+*/
+
+// create a root reducer for all the reducers to combine them
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+// persist reducer configuration
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+// create the persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
+  // user: userReducer,
+  // use the persisted reducer
+  // prevent errors [ https://redux-toolkit.js.org/api/getDefaultMiddleware ]
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
+
+// export the persistedstore
+export const persistor = persistStore(store);
